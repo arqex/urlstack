9a439a927e285d95911ffbfc372dc34fa17efbd0
Javier Marquez <javi@arqex.com>
2018-10-22 00:27:59 +0200

    adds transitions to the narrow navigator

--- src/NavigatorNarrow.js
+++ src/NavigatorNarrow.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Animated, Easing } from 'react-native'
 import ScreenWrapperNarrow from './ScreenWrapperNarrow'
 import SideMenu from 'react-native-side-menu'
 
 export default class NavigatorNarrow extends Component {
+	constructor(props) {
+		super(props);
+
+		let { stack, currentIndex } = props.router
+
+		this.currentIndex = currentIndex
+
+		this.setTransitions(stack, currentIndex)
+	}
+
 	render(){
 		let { router, DrawerComponent } = this.props;
 
 		let content = (
 			<View style={ styles.container }>
-				<View style={ styles.stack }>
-					{ this.renderScreenWrapper( router ) }
+				<View style={styles.stack}>
+					{ this.renderScreens(router) }
 				</View>
 				<View style={ styles.modal }>
 				</View>
@@ -48,6 +58,7 @@ export default class NavigatorNarrow extends Component {
 				router={ router }
 				location={ location }
 				isActive={ router.currentIndex === i }
+				transition={this.transitions[key]}
 				drawer={ this.getDrawer() }
 				key={ key } />
 		))
@@ -69,6 +80,65 @@ export default class NavigatorNarrow extends Component {
 			return this.drawer;
 		}
 		return emptyDrawer;
+	}
+	componentWillReceiveProps(nextProps) {
+		let { stack } = nextProps.router;
+		this.setTransitions(stack, this.currentIndex)
+	}
+
+	componentDidUpdate() {
+		let { stack, currentIndex } = this.props.router;
+		if (this.currentIndex !== currentIndex) {
+			this.currentIndex = currentIndex
+			this.updateTransitions(stack, currentIndex)
+		}
+	}
+
+	setTransitions(stack, currentIndex) {
+		if (!this.transitions) {
+			this.transitions = {}
+		}
+
+		// Track what transitions are not used anymore
+		let oldTransitions = { ...this.transitions }
+
+		stack.forEach(({ key }, i) => {
+			if (this.transitions[key]) {
+				return delete oldTransitions[key]
+			}
+
+			let value = 0;
+			if (currentIndex === i) {
+				value = 100;
+			}
+			else if (i < currentIndex) {
+				value = 200;
+			}
+			this.transitions[key] = new Animated.Value(value)
+		})
+
+		// Delete tranistions not used
+		Object.keys(oldTransitions).forEach(key => {
+			delete this.transitions[key]
+		})
+	}
+
+	updateTransitions(stack, currentIndex) {
+
+		stack.forEach(({ key }, i) => {
+			let t = this.transitions[key]
+			if (currentIndex === i && t._value !== 100) {
+				Animated.timing(t, { toValue: 100, easing: Easing.linear, duration: 1000 })
+			}
+			else if (currentIndex < i && t._value !== 200) {
+				Animated.timing(t, { toValue: 200, easing: Easing.linear, duration: 1000 })
+			}
+			else if (currentIndex > i && t._value !== 0) {
+				Animated.timing(t, { toValue: 0, easing: Easing.linear, duration: 1000 })
+			}
+		})
+
+		console.log(this.transitions)
 	}
 }
 
