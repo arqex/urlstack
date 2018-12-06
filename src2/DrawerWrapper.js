ddac34d9944dadb7d681c9056903b8e8ef9eb413
Javier Marquez <javi@arqex.com>
2018-12-06 18:39:35 +0100

    Collapsible drawer working ok

--- src2/DrawerWrapper.js
+++ src2/DrawerWrapper.js
@@ -7,6 +7,21 @@ export default class DrawerWrapper extends Component {
 	constructor(props){
 		super(props)
 		this.drawerWidth = 300;
+
+		this.drawerPos = new Animated.Value(0);
+		this.calculateDrawerIndex();
+		this.open = false;
+
+		this.overlayAnimStyle = {
+			transform: [{translateX: this.drawerIndex.interpolate({
+				inputRange: [0, 0.01, 1],
+				outputRange: [ -10000, 0, 0]
+			})}],
+			opacity: this.drawerIndex.interpolate({
+				inputRange: [0,0,1,1],
+				outputRange: [0,0,.5,.5]
+			})
+		}
 	}
 
 	render(){
@@ -21,20 +36,25 @@ export default class DrawerWrapper extends Component {
 		]		
 		let overlayStyles = [
 			styles.overlay,
-			collapsible && styles.collapsibleOverlay
+			this.overlayAnimStyle,
+			collapsible && styles.collapsibleOverlay,
 		]
 
 		let snapPoints = [
-			{ x: 0 }, {x: this.drawerWidth}
+			{ x: 0, id: 'closed' }, {x: this.drawerWidth, id: 'open'}
 		];
 
 		return (
 			<Animated.View style={ containerStyles }>
-				<View style={ overlayStyles }></View>
-				<Interactable.View dragEnabled={ collapsible } horizontalOnly={ true } snapPoints={ snapPoints }>
-					<View style={ drawerStyles }>
-						<View style={ styles.drawerBg } />
-						<Drawer router={ router } onLayout={ e => this.updateLayout(e) } />
+				<Animated.View style={ overlayStyles } onClick={ () => this.closeDrawer() }></Animated.View>
+				<Interactable.View dragEnabled={ collapsible }
+					ref="drawer"
+					horizontalOnly={ true } snapPoints={ snapPoints }
+					boundaries={{right: this.drawerWidth, bounce: 0.5}}
+					onSnapStart={ e => this.onSnap( e ) }
+					animatedValueX={ this.drawerPos }>
+					<View style={ drawerStyles } onLayout={ e => this.updateLayout(e) }>
+						<Drawer router={ router } />
 						<View style={ styles.dragHandle } />
 					</View>
 				</Interactable.View>
@@ -43,26 +63,60 @@ export default class DrawerWrapper extends Component {
 	}
 	
 	updateLayout( e ){
-		let {layout} = e.nativeEvent.layout;
+		let {layout} = e.nativeEvent;
 
 		this.animatedStyles = animatedStyles(this.props.transition, this.props.indexes, layout );
 		this.drawerWidth = layout.width;
+		this.calculateDrawerIndex();
 		this.forceUpdate();
+	}
+
+	calculateDrawerIndex(){
+		let di = this.drawerIndex;
+		let index = this.drawerPos.interpolate({
+			inputRange: [0, this.drawerWidth ],
+			outputRange: [0, 1]
+		})
+
+		if( di ){
+			di._config = index._config;
+			di._interpolation = index._interpolation;
+		}
+		else {
+			this.drawerIndex = index;
+		}
+	}
+
+	openDrawer(){
+		if( !this.props.collapsible || this.open ) return;
+
+		let drawer = this.refs.drawer
+		drawer && drawer.setVelocity({x: 2000})
+	}
+
+	closeDrawer(){
+		if( !this.props.collapsible || !this.open ) return;
+
+		let drawer = this.refs.drawer
+		drawer && drawer.setVelocity({x: -2000})
+	}
+
+	onSnap( e ){
+		if( e.nativeEvent ) e = e.nativeEvent
+		this.open = e.id === 'open';
 	}
 }
 
 let styles = StyleSheet.create({
 	container: {
-    position: 'absolute',
-		top: 0, bottom: 0, left: 0,
-		width: '100%',
-    flexDirection: 'row-reverse',
-		transform: [{ translateX: '-100%'}],
-		zIndex: 2000,
-		backgroundColor: '#e0e0e0',
 	},
 	collapsibleDrawer: {
-		
+    	position: 'absolute',
+		top: 0, bottom: 0, left: '-100%',
+		width: '100%',
+    	flexDirection: 'row-reverse',
+		zIndex: 2000,
+		backgroundColor: '#e0e0e0',
 	},
 	drawer: {
     left: 0,
@@ -81,17 +135,16 @@ let styles = StyleSheet.create({
 	dragHandle: {
 		width: 40,
 		top: 0, bottom: 0, right: -20,
-		backgroundColor: 'green',
+		// backgroundColor: 'green',
 		position: 'absolute',
 		zIndex: 10
 	},
 	overlay: {
-		backgroundColor: 'red',
+		backgroundColor: 'black',
 		height: '100%',
 		width: '100%',
 		position: 'absolute',
-		left: '100%',
-		zIndex: -1
+		left: '100%'
 	},
 	collapsibleOverlay: {
 	}
